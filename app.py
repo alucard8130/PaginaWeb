a7b1383cd54de6917dcc77646f5c8f41f3c45868
Alucard8130 <smart8130@hotmail.com>
2025-08-31 14:54:23 -0600

    mejoras

--- app.py
+++ app.py
@@ -78,9 +78,9 @@ def redes():
     return render_template("redes.html")
 
 
-@app.route("/gestor_condominal")
-def gestor_condominal():
-    return render_template("gestor_condominal.html")
+@app.route("/gestor_administrativo")
+def gestor_administrativo():
+    return render_template("gestor_administrativo.html")
 
 
 # Ruta para registro de curso
